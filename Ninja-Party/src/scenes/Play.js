dad5ddcc025b82e12a33784e330caf275ff3808e
ianjustinferris <ianjustinferris@gmail.com>
2022-06-08 00:57:35 -0400

    Added background layers with camera scroll

---
+++ Ninja-Party/src/scenes/Play.js
@@ -0,0 +1,53 @@
+import Phaser from 'phaser';
+class Play extends Phaser.Scene {
+
+    constructor() {
+        super('PlayScene');
+    }
+
+    preload() {
+        this.cursors = this.input.keyboard.createCursorKeys()
+
+        this.load.image('layer1', 'assets/Layer_1.png');
+        this.load.image('layer2', 'assets/Layer_2.png');
+        this.load.image('layer3', 'assets/Layer_3.png');
+        this.load.image('layer4', 'assets/Layer_4.png');
+        this.load.image('layer5', 'assets/Layer_4Lights.png');
+        this.load.image('layer6', 'assets/Layer_04Lights.png');
+        this.load.image('layer7', 'assets/Layer_5.png');
+        this.load.image('layer8', 'assets/Layer_6.png');
+        this.load.image('layer9', 'assets/Layer_7.png');
+        this.load.image('layer10', 'assets/Layer_8.png');
+        this.load.image('layer11', 'assets/Layer_9.png');
+    }
+
+    create() {
+        this.add.image(.5, .5, 'layer1').setOrigin(0);
+        this.add.image(.5, .5, 'layer2').setOrigin(0);
+        this.add.image(.5, .5, 'layer3').setOrigin(0);
+        this.add.image(.5, .5, 'layer4').setOrigin(0);
+        this.add.image(.5, .5, 'layer5').setOrigin(0);
+        this.add.image(.5, .5, 'layer6').setOrigin(0);
+        this.add.image(.5, .5, 'layer7').setOrigin(0);
+        this.add.image(.5, .5, 'layer8').setOrigin(0);
+        this.add.image(.5, .5, 'layer9').setOrigin(0);
+        this.add.image(.5, .5, 'layer10').setOrigin(0);
+        this.add.image(.5, .5, 'layer11').setOrigin(0);
+    }
+
+
+
+    update() {
+        const cam = this.cameras.main
+        const speed = 3
+        if (this.cursors.left.isDown) {
+            //moveLeft
+            console.log('left')
+            cam.scrollX -= speed
+        } else if (this.cursors.right.isDown) {
+            cam.scrollX += speed
+            console.log('right')
+        }
+    }
+}
+export default Play;
